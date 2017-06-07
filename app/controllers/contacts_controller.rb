687befbac1964e443f8e5f37a0177cd6d8750057
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-07 10:30:34 -0700

    adde CRUD capabilities for contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,48 @@
 class ContactsController < ApplicationController
+    before_action :set_contact, only: [:show, :edit, :update, :destroy]
+
     def index
+        @contacts = Contact.all
     end
 
     def new
+        @contact = Contact.new
+    end
+
+    def create
+        if @contact = Contact.create(contact_params)
+            redirect_to contacts_path
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @contact.update(contact_params)
+            redirect_to(contact_path(@contact))
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @contact.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def contact_params
+        params.require(:contact).permit(:name, :email, :secondary_email, :cell, :work, :company, :role, :linkedin, :facebook, :notes)
+    end
+
+    def set_contact
+        @contact = Contact.find(params[:id])
     end
 end
