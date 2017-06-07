dd95758219098eea90dcbeaed341ed0587f2262b
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-07 10:58:26 -0700

    added error flashing

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,8 +11,10 @@ class ContactsController < ApplicationController
 
     def create
         if @contact = Contact.create(contact_params)
+            flash[:success] = "Contact successfully created."
             redirect_to contacts_path
         else
+            flash.now[:alert] = "Failed to add contac. Please check the form."
             render :new
         end
     end
@@ -25,14 +27,17 @@ class ContactsController < ApplicationController
 
     def update
         if @contact.update(contact_params)
+            flash[:success] = "Contact updated."
             redirect_to(contact_path(@contact))
         else
+            flash.now[:alert] = "Update failed. Please check the form."
             render :edit
         end
     end
 
     def destroy
         @contact.destroy
+        flash[:success] = "Contact successfully deleted."
         redirect_to root_path
     end
 
