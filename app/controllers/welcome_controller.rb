350b8a2aa7fd784c1ce4f5269bbdc02b25286928
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-07 14:34:19 -0700

    added tether

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-      if (current_user)
-          redirect_to 'contacts#index'
-      end
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
     validates :user_id, presence: true
-    belongs_to: user
+    belongs_to :user
     has_many :reaches, dependent: :destroy
 end
