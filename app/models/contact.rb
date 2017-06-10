0a462f3e6cb892bf8c05b1a80d8912b51a43c4ec
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-10 16:45:34 -0700

    added validations for contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,9 @@
 class Contact < ApplicationRecord
     validates :user_id, presence: true
+    validates :name, presence: true
+    validates :company, presence: true
+    validates :role, presence: true
+    
     belongs_to :user
     has_many :reaches, dependent: :destroy
 end
