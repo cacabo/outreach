9a1e56cce8f80be395fde85f8eac4663b397e3f0
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-10 22:21:59 -0700

    updated validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,9 @@
 class Contact < ApplicationRecord
     validates :user_id, presence: true
-    validates :name, presence: true
-    validates :company, presence: true
-    validates :role, presence: true
-    
+    validates :name, presence: true { message: "Name cannot be blank" }
+    validates :company, presence: true, message: "Company connot be blank"
+    validates :role, presence: true, message: "Role cannot be blank"
+
     belongs_to :user
     has_many :reaches, dependent: :destroy
 end
