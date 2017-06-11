b0ce07c4059414fceffa058c7f37c3715c96cc6f
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-10 22:24:21 -0700

    fixed issue with validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,8 @@
 class Contact < ApplicationRecord
     validates :user_id, presence: true
-    validates :name, presence: true { message: "Name cannot be blank" }
-    validates :company, presence: true, message: "Company connot be blank"
-    validates :role, presence: true, message: "Role cannot be blank"
+    validates :name, presence: true
+    validates :company, presence: true
+    validates :role, presence: true
 
     belongs_to :user
     has_many :reaches, dependent: :destroy
